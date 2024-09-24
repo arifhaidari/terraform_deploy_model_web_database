84f351ae6f0ff7d9c1a88a84f3bd4ace14231c0d
Arif Haidari <arifhaidari336@gmail.com>
2024-09-24 15:42:05 +0200

    deployment using helm

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Terraform
 
-Deploy websites and databases
+Deployed websites and databases
+
+we did deploy using kubernetes provider.
+
+We will now use Terraform's helm provider to deploy the same WordPress application on the Kubernetes cluster, but this time using Helm charts.
--- kubernetes/provider.tf
+++ kubernetes/provider.tf
@@ -4,10 +4,22 @@ terraform {
       source = "hashicorp/kubernetes"
       version = "~> 1.6.1"
     }
+     helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.8"
+    }
   }
 }
 
+# Kubernetes provider
 provider "kubernetes" {
   config_path = "~/.kube/config" # kubernetes configuration file path
+}
+
+# Helm provider to manage Helm charts in Kubernetes
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"  # Path to the Kubernetes config used by Helm
+  }
 }
 
